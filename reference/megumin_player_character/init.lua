425448fc518fc95b00976014d28f0b632c51223a
JKRyan <jed@jedkendall.com>
2019-11-14 10:43:43 -0600

    Initial commit and base files added for more convenient work.

---
+++ reference/megumin_player_character/init.lua
@@ -0,0 +1,11 @@
+function OnModPreInit()
+	print("Mod - OnModPreInit()") -- first this is called for all mods
+end
+
+function OnModInit()
+	print("Mod - OnModInit()") -- after that this is called for all mods
+end
+
+function OnModPostInit()
+	print("Mod - OnModPostInit()") -- then this is called for all mods
+end
